fd5b38d969aedfb5a5b01064e6a20bec87fc7d8c
Meowitzher <meowitzher@gmail.com>
2022-06-13 13:04:24 -0400

    Docs Readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,69 @@
+//! [![Crates.io](https://img.shields.io/crates/v/sepax2d.svg)](https://crates.io/crates/sepax2d)
+//! [![MIT/Apache 2.0](https://img.shields.io/badge/license-MIT%2FApache-blue.svg)](./LICENSE)
+//! 
+//! # sepax2d
+//! A safe Rust crate for finding and resolving collisions of convex shapes using the Separating Axis Theorem (SAT) in two dimensions.
+//! 
+//! ### Usage
+//! 
+//! Add the following to the `[dependencies]` section of your `Cargo.toml`:
+//! 
+//! ```toml
+//! sepax2d = "0.3"
+//! ```
+//! 
+//! Import the types of shapes you'd like to use and create some new shapes:
+//! 
+//! ```rust
+//! use sepax2d::prelude::*;
+//! 
+//! let rectangle = AABB::new(top_left, width, height);
+//! let circle = Circle::new(center, radius);
+//! let capsule = Capsule::new(center, arm, radius);
+//! 
+//! //The vertices of a polygon are position vectors
+//! //relative to the shape's position, i.e. if position 
+//! //is (1, 2), then the absolute location of the 
+//! //first vertex is (1, 0).
+//! let triangle = Polygon::from_vertices
+//! (
+//!     
+//!     position,
+//!     vec![(0.0, -2.0), (-1.0, 2.0), (1.0, 2.0)]
+//! 
+//! );
+//! ```
+//! 
+//! Use the `sat_overlap(&left, &right)` method to get a `bool` indicating whether or not the two given shapes overlap.
+//! Any struct implementing the `Shape` trait can be used.
+//! 
+//! ```rust
+//! assert!(sat_overlap(&circle, &capsule));
+//! assert!(sat_overlap(&triangle, &rectangle));
+//! ```
+//! 
+//! Use the `sat_collision(&left, &right)` method to get a `(f32, f32)` which represents the shift needed to add to `right`'s
+//! position in order to resolve the overlap of the two shapes.
+//! 
+//! ```rust
+//! let resolution = sat_overlap(&circle, &triangle);
+//! 
+//! let position = triangle.position();
+//! triangle.set_position((position.0 + resolution.0, position.1 + resolution.1));
+//! 
+//! assert!(!sat_overlap(&circle, &triangle));
+//! ```
+//! 
+//! Use the `contains_point(&shape, point)` method to get a `bool` indicating whether or not the specified point
+//! is inside the given shape.
+//! 
+//! ```rust
+//! let rectangle = AABB::new((0.0, 0.0), 5.0, 2.0);
+//! 
+//! assert!(contains_point(&rect, (1.0, 1.0)));
+//! assert!(!contains_point(&rect, (10.0, -1.0)));
+//! ```
+
 #![allow(clippy::needless_return)]
 
 pub mod polygon;
